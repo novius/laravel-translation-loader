07f53e61f4c080dc86c02734301ba404a1b9f803
Gilles Felix <felix@novius.fr>
2025-05-14 14:36:30 +0200

    fix actions typage

--- src/Actions/DbToFile.php
+++ src/Actions/DbToFile.php
@@ -2,7 +2,7 @@
 
 namespace Novius\TranslationLoader\Actions;
 
-use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\File;
 use Lorisleiva\Actions\Concerns\AsObject;
 use Novius\TranslationLoader\LanguageLine;
